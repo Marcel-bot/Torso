f450a6521745ab96c70eceb7cbc0be9c24255b40
Karo8870 <cazacuchristian@gmail.com>
2024-07-29 18:19:08 +0300

    Added direct communication with right hand (Without pinging)

--- include/main.h
+++ include/main.h
@@ -2,5 +2,10 @@
 # define MAIN
 
 #include <Arduino.h>
+#include <Wire.h>
+
+#define BAUDRATE 115200
+
+#define RIGHT_HAND_ADDRESS 0x08
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <main.h>
 
 void setup() {
-
+  Serial.begin(BAUDRATE);
+  Wire.begin();
 }
 
 void loop() {
-
+  if (Serial.available()) {
+    Wire.beginTransmission(RIGHT_HAND_ADDRESS);
+    Wire.write(Serial.read());
+    Wire.endTransmission();
+  }
 }
